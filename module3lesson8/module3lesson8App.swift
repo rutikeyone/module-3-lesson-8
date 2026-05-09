402ccd25e7d106f56d5a238dc710e4fedbfa954d
Ершов Андрей <rutikeyone@gmail.com>
2026-05-09 15:53:10 +0300

    Initial Commit

---
+++ module3lesson8/module3lesson8App.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson8App.swift
+//  module3lesson8
+//
+//  Created by Andrew on 09.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
