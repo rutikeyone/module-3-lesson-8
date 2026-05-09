e34c1883f40ec3b074482149757b31b634bb4ea3
Ершов Андрей <rutikeyone@gmail.com>
2026-05-09 21:35:55 +0300

    Homework 8 from module 3 has been added

--- module3lesson8/ContentView.swift
+++ module3lesson8/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var opacity: CGFloat = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: .top) {
+            HStack {
+                Text("Title")
+                    .padding()
+                    .foregroundStyle(.accentText)
+            }
+            .frame(maxWidth: .infinity)
+            .background(.primaryTitle)
+            .zIndex(1)
+            .opacity(opacity)
+            
+            ScrollView {
+                VStack {
+                    GeometryReader { proxy in
+                        let minY = proxy.frame(in: .global).minY
+                        
+                        let height = minY > 0 ? minY : 0
+                        let offsetY = -minY < 0 ? -minY : 0
+                        
+                        Image(.programming)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: proxy.size.width)
+                            .frame(height: 300 + height)
+                            .offset(y: offsetY)
+                            .onChange(of: minY) { _, newValue in
+                                opacity = -minY / 300
+                            }
+                    }
+                    .frame(height: 300)
+                }
+                
+                LazyVStack(spacing: 16) {
+                    ForEach(0..<100, id: \.self) { item in
+                        Rectangle()
+                            .frame(height: 50)
+                            .foregroundStyle(.green.opacity(0.4))
+                            .overlay {
+                                Text("\(item)")
+                            }
+                    }
+                }
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .ignoresSafeArea(edges: .top)
         }
-        .padding()
     }
 }
 
